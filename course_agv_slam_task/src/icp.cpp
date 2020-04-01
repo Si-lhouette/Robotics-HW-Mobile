f374005cf19c90ea923cde6a5eae11c8b08f703d
Si-lhouette <3170105242@zju.edu.cn>
2020-04-01 23:05:52 +0800

    icp update function

--- course_agv_slam_task/src/icp.cpp
+++ course_agv_slam_task/src/icp.cpp
@@ -203,120 +203,100 @@ void icp::process(sensor_msgs::LaserScan input)
     }
 
     src_pc = this->rosmsgToEigen(input);
+
+    if(src_pc.cols() < 100){
+        cout<<"Too Little points!!!!!!!!!"<<endl;
+        return;
+    }
+
+
     MatrixXd src_pc_copy;
     src_pc_copy = src_pc;
     cout<<src_pc.cols()<<endl;
-    if(src_pc.cols() > 100){
-        double time_0 = (double)ros::Time::now().toSec();
 
-        // init some variables
-        Eigen::Matrix3d Transform_acc = Eigen::MatrixXd::Identity(3,3);
-        Eigen::Matrix2d R_all = Eigen::MatrixXd::Identity(2,2);
-        Eigen::Vector2d T_all;
-        T_all << 0.0,0.0,0.0;
-        
-        calcinitRT(R_all, T_all);
+
+    double time_0 = (double)ros::Time::now().toSec();
+
+    // init some variables
+    Eigen::Matrix3d Transform_acc = Eigen::MatrixXd::Identity(3,3);
+    Eigen::Matrix2d R_all = Eigen::MatrixXd::Identity(2,2);
+    Eigen::Vector2d T_all;
+    T_all << 0.0,0.0,0.0;
+    Eigen::Matrix2d R_12;
+    Eigen::Vector2d T_12;
+    double elast = 0.0;
+    
+    calcinitRT(R_all, T_all);
+    Transform_acc.block(0,0,2,2) = R_all;
+    Transform_acc.block(0,2,2,1) = T_all;
+    src_pc = Transform_acc*src_pc_copy;
+
+
+
+    // main LOOP
+    for(int i=0; i<max_iter; i++)
+    {   
+        cout<<"loop_"<<i<<endl;
+        //1. Find neareast correspond
+        NeighBor s_t_near;
+        s_t_near = findNearest(kdtreeLast.makeShared(), src_pc);
+
+        //cout<<"findNearest Done"<<endl;
+        MatrixXd tar_pcn;
+        MatrixXd src_pcn;
+
+
+        int s_num = s_t_near.src_indices.size();
+
+        src_pcn = Eigen::MatrixXd::Constant(3, s_num, 1);
+        tar_pcn = Eigen::MatrixXd::Constant(3, s_num, 1);
+        for(int j = 0; j < s_num; j++){
+            src_pcn.col(j) = src_pc.col(s_t_near.src_indices[j]);
+            tar_pcn.col(j) = tar_pc.col(s_t_near.tar_indices[j]);
+        }
+
+
+        //2. Solve for RotationMatrix & translation vector
+        Matrix3d T = getTransform(src_pcn, tar_pcn);
+        R_12 = T.block(0,0,2,2);
+        T_12 = T.block(0,2,2,1);
+
+        //3. Updata R&T
+        T_all = R_12 * T_all + T_12;
+        R_all = R_12 * R_all;
         Transform_acc.block(0,0,2,2) = R_all;
         Transform_acc.block(0,2,2,1) = T_all;
         src_pc = Transform_acc*src_pc_copy;
 
 
+        //4. Cheak error tolerance
 
-        // main LOOP
-        for(int i=0; i<max_iter; i++)
-        {   
+        double e = accumulate(s_t_near.sqared_dis.begin(), s_t_near.sqared_dis.end(), 0.0) / s_num;
+        cout<<"e:"<<e<<endl;
+        //cout<<"de:"<<fabs((e - elast)/elast)<<endl;
+        if(e < tolerance || fabs((e - elast)/elast) <0.001){
             cout<<"loop_"<<i<<endl;
-            //1. Find neareast correspond
-            NeighBor s_t_near;
-            s_t_near = findNearest(kdtreeLast.makeShared(), src_pc);
-
-
-
-            //cout<<"findNearest Done"<<endl;
-            MatrixXd tar_pcn;
-            MatrixXd src_pcn;
-
-
-            int s_num = s_t_near.src_indices.size();
-
-            src_pcn = Eigen::MatrixXd::Constant(3, s_num, 1);
-            tar_pcn = Eigen::MatrixXd::Constant(3, s_num, 1);
-            for(int j = 0; j < s_num; j++){
-                src_pcn.col(j) = src_pc.col(s_t_near.src_indices[j]);
-                tar_pcn.col(j) = tar_pc.col(s_t_near.tar_indices[j]);
-            }
-
-
-            //2. Solve for RotationMatrix & translation vector
-            double tx_mean = tar_pcn.row(0).mean();
-            double ty_mean = tar_pcn.row(1).mean();
-            tar_pcn.row(0) -=  Eigen::MatrixXd::Constant(1, s_num, tx_mean);
-            tar_pcn.row(1) -=  Eigen::MatrixXd::Constant(1, s_num, ty_mean);
-
-            double sx_mean = src_pcn.row(0).mean();
-            double sy_mean = src_pcn.row(1).mean();
-            src_pcn.row(0) -=  Eigen::MatrixXd::Constant(1, s_num, sx_mean);
-            src_pcn.row(1) -=  Eigen::MatrixXd::Constant(1, s_num, sy_mean); 
-
-            MatrixXd W = Eigen::MatrixXd::Zero(2,2);
-            W = src_pcn.topRows(2) * tar_pcn.topRows(2).transpose();
-
-            // SVD on W
-            Eigen::JacobiSVD<Eigen::Matrix2d> svd(W, Eigen::ComputeFullU | Eigen::ComputeFullV);
-            Eigen::Matrix2d U = svd.matrixU();
-            Eigen::Matrix2d V = svd.matrixV();
-
-            Eigen::Matrix2d R_12 = V* (U.transpose());
-            Eigen::Vector2d T_12 = Eigen::Vector2d(tx_mean, ty_mean) - R_12 * Eigen::Vector2d(sx_mean, sy_mean);
-
-            //3. Updata R&T
-            T_all = R_12 * T_all + T_12;
-            R_all = R_12 * R_all;
-            Transform_acc.block(0,0,2,2) = R_all;
-            Transform_acc.block(0,2,2,1) = T_all;
-            src_pc = Transform_acc*src_pc_copy;
-
-
-            //4. Cheak error tolerance
-
-            double e = accumulate(s_t_near.sqared_dis.begin(), s_t_near.sqared_dis.end(), 0.0) / s_num;
-            if(i<=70){
-                cout<<"e:"<<e<<endl;
-            }
-            if(e < tolerance){
-                cout<<"loop_"<<i<<endl;
-                cout<<"e:"<<e<<endl;
-                break;
-            }
-
+            cout<<"e:"<<e<<endl;
+            //break;
         }
-
-
-        tar_pc = this->rosmsgToEigen(input);
-        buildKdtree(tar_pc);
-
-
-
-        Transform_acc.block(0,0,2,2) = R_all;
-        Transform_acc.block(0,2,2,1) = T_all;
-
-        this->publishResult(Transform_acc);
-
-        double time_1 = (double)ros::Time::now().toSec();
-        cout<<"time_cost:  "<<time_1-time_0<<endl;
-
-
-
-
-
+        elast = e;
 
     }
-    else{
-        cout<<"Too Little points!!!!!!!!!"<<endl;
-    }
 
 
 
+    tar_pc = src_pc_copy;
+    buildKdtree(tar_pc);
+
+
+
+    Transform_acc.block(0,0,2,2) = R_all;
+    Transform_acc.block(0,2,2,1) = T_all;
+
+    this->publishResult(Transform_acc);
+
+    double time_1 = (double)ros::Time::now().toSec();
+    cout<<"time_cost:  "<<time_1-time_0<<endl;
 
 }
 
@@ -361,10 +341,39 @@ NeighBor icp::findNearest(pcl::KdTreeFLANN<pcl::PointXYZ>::Ptr kdtreeLast, const
     return s_t_near;
 }
 
-// Eigen::Matrix3d icp::getTransform(const Eigen::MatrixXd &src, const Eigen::MatrixXd &tar)
-// {
-//     // TODO: please code by yourself
-// }
+Eigen::Matrix3d icp::getTransform(const Eigen::MatrixXd &src, const Eigen::MatrixXd &tar)
+{
+
+    Eigen::MatrixXd src_rec = src;
+    Eigen::MatrixXd tar_rec = tar;
+
+    int s_num = tar_rec.cols();
+    double tx_mean = tar_rec.row(0).mean();
+    double ty_mean = tar_rec.row(1).mean();
+    tar_rec.row(0) -=  Eigen::MatrixXd::Constant(1, s_num, tx_mean);
+    tar_rec.row(1) -=  Eigen::MatrixXd::Constant(1, s_num, ty_mean);
+
+    double sx_mean = src_rec.row(0).mean();
+    double sy_mean = src_rec.row(1).mean();
+    src_rec.row(0) -=  Eigen::MatrixXd::Constant(1, s_num, sx_mean);
+    src_rec.row(1) -=  Eigen::MatrixXd::Constant(1, s_num, sy_mean); 
+
+    MatrixXd W = Eigen::MatrixXd::Zero(2,2);
+    W = src_rec.topRows(2) * tar_rec.topRows(2).transpose();
+
+    // SVD on W
+    Eigen::JacobiSVD<Eigen::Matrix2d> svd(W, Eigen::ComputeFullU | Eigen::ComputeFullV);
+    Eigen::Matrix2d U = svd.matrixU();
+    Eigen::Matrix2d V = svd.matrixV();
+
+    Eigen::Matrix2d R_12 = V* (U.transpose());
+    Eigen::Vector2d T_12 = Eigen::Vector2d(tx_mean, ty_mean) - R_12 * Eigen::Vector2d(sx_mean, sy_mean);
+
+    Matrix3d T;
+    T << R_12, T_12,
+         0, 0, 1;
+    return T;
+}
 
 // float icp::calc_dist(const Eigen::Vector2d &pta, const Eigen::Vector2d &ptb)
 // {
